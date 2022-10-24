33c8a3a601c3080d0e6108ba3bebf85e92a42d87
Stephen Severino <59100054+muffledcry@users.noreply.github.com>
2022-10-24 12:30:17 +0000

    Finished tax_rate function, added calculation function

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
-#We need to know the sales tax rate
+
+
+
 def set_tax_rate():
-  #To Do:
-  #1. Add all 50 states + Puerto Rico
-  #2. Grab the tax rate from the dictionary using its abbreviation.
-  #3. Return that tax rate for use elsewhere in our code.
   state = input("Enter the abbreviation for your state:\n")
   tax_dict = {"NJ":0.066, 
               "DE":0.00,
               "OR":0.00,
              }
+  tax_rate = tax_dict[state]
+  return tax_rate
 
-#We need to get the price of the item
 def get_item():
   item_name = input("What item are you buying?")
   price = float(input("What is the price of the item?"))
   return item_name, price
 
+def calculate_tax(price, tax_rate):
+  global tax_rate
+  sales_tax = price * tax_rate
+  total = price + sales_tax
+  return total, sales_tax
